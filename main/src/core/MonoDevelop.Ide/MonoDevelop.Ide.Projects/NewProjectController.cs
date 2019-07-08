mjbond-msft/monodevelop

commit 1578ca3ed2ebfc96890e23fe580f6ede097221b8

[Ide] Close the New Project Dialog right after creation
The new wproject dialog can be closed once all template
bits have been processed and there is no way back.

This avoids collisions with other dialogs and alerts which
might pop up when the new project is loaded for the
first time, expecting the main IDE window to be visible
and not blocked.

Fixes VSTS #854324

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/NewProjectController.cs
@@ -674,6 +674,8 @@ namespace MonoDevelop.Ide.Projects
 				}
 			}
 
+			dialog.CloseDialog ();
+
 			if (ParentFolder != null)
 				await IdeApp.ProjectOperations.SaveAsync (ParentFolder.ParentSolution);
 			else
@@ -712,8 +714,6 @@ namespace MonoDevelop.Ide.Projects
 				tcs.SetResult (true);
 			});
 			await tcs.Task;
-
-			dialog.CloseDialog ();
 		}
 
 		public WizardPage CurrentWizardPage {

commit b836697dd443b2814a0b1c755a12a9c7ef11374b

[GetToCode] Adds ShowAsync method in NewProjectController with TaskCompletionSource This PR fixes a problem waiting the response from NewProject window probably introduced by 1578ca3#diff-e3e1db8132f317c043c737963f36d108
The problem is now we close the dialog very quickly without wait the creation of the project, then the variable is modified after return the value.

This is necessary to fix, because GTC in case of project creation needs to disable the window to don't allow the user to click buttons meanwhile we open the project.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/NewProjectController.cs
@@ -54,6 +54,7 @@ namespace MonoDevelop.Ide.Projects
 	{
 		public event EventHandler ProjectCreationFailed;
 		public event EventHandler ProjectCreated;
+		public event EventHandler ProjectCreationWindowClosed;
 
 		string chooseTemplateBannerText =  GettextCatalog.GetString ("Choose a template for your new project");
 		string configureYourWorkspaceBannerText = GettextCatalog.GetString ("Configure your new workspace");
@@ -164,6 +165,7 @@ namespace MonoDevelop.Ide.Projects
 			GetVersionControlHandler ();
 		}
 
+		[Obsolete ("Use ShowAsync () instead")]
 		public bool Show ()
 		{
 			projectConfiguration.CreateSolution = ParentFolder == null;
@@ -178,6 +180,28 @@ namespace MonoDevelop.Ide.Projects
 			dialog.RegisterController (this);
 
 			dialog.ShowDialog ();
+
+			imageProvider.Dispose ();
+
+			return IsNewItemCreated;
+		}
+
+		public async Task<bool> ShowAsync ()
+		{
+			projectConfiguration.CreateSolution = ParentFolder == null;
+			LoadTemplateCategories ();
+			SetDefaultSettings ();
+			SelectDefaultTemplate ();
+
+			CreateFinalConfigurationPage ();
+			CreateWizardProvider ();
+
+			dialog = CreateNewProjectDialog ();
+			dialog.RegisterController (this);
+
+			dialog.ShowDialog ();
+
+			await EnsureCreationFinishedAsync ();
 
 			imageProvider.Dispose ();
 
@@ -613,6 +637,8 @@ namespace MonoDevelop.Ide.Projects
 
 		public async Task Create ()
 		{
+			projectCreated = new TaskCompletionSource<bool> ();
+
 			if (wizardProvider.HasWizard)
 				wizardProvider.BeforeProjectIsCreated ();
 
@@ -675,7 +701,8 @@ namespace MonoDevelop.Ide.Projects
 			}
 
 			dialog.CloseDialog ();
-
+			ProjectCreationWindowClosed?.Invoke (this, EventArgs.Empty);
+		
 			if (ParentFolder != null)
 				await IdeApp.ProjectOperations.SaveAsync (ParentFolder.ParentSolution);
 			else
@@ -709,13 +736,21 @@ namespace MonoDevelop.Ide.Projects
 			IsNewItemCreated = true;
 			UpdateDefaultSettings ();
 
-			var tcs = new TaskCompletionSource<bool> ();
 			Gtk.Application.Invoke ((sender, args) => {
 				ProjectCreated?.Invoke (this, EventArgs.Empty);
-				tcs.SetResult (true);
+				projectCreated.SetResult (true);
 			});
-			await tcs.Task;
+			await projectCreated.Task;
 		}
+
+		async Task EnsureCreationFinishedAsync ()
+		{
+			if (projectCreated?.Task == null)
+				return;
+			await projectCreated.Task;
+		}
+
+		TaskCompletionSource<bool> projectCreated;
 
 		public WizardPage CurrentWizardPage {
 			get {

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/ProjectOperations.cs
@@ -710,10 +710,19 @@ namespace MonoDevelop.Ide
 
 			IdeApp.Workbench.EnsureLayout ();
 			var newProjectDialog = new NewProjectDialogController ();
+
+			static void projectCreationWindowClosed (object s, EventArgs e)
+			{
+				WelcomePage.WelcomePageService.HideWelcomePageOrWindow ();
+			}
+			newProjectDialog.ProjectCreationWindowClosed += projectCreationWindowClosed;
+
 			newProjectDialog.OpenSolution = true;
 			newProjectDialog.SelectedTemplateId = defaultTemplate;
 			newProjectDialog.ShowTemplateSelection = showTemplateSelection;
-			return newProjectDialog.Show ();
+			var dialogResult = await newProjectDialog.ShowAsync ();
+			newProjectDialog.ProjectCreationWindowClosed -= projectCreationWindowClosed;
+			return dialogResult;
 		}
 		
 		public Task<WorkspaceItem> AddNewWorkspaceItem (Workspace parentWorkspace)